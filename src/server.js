97d4f547b6fd9d8bc0d43147307a33d95efc18a0
Thaitheyasudan P K <91272336+Thaitheya@users.noreply.github.com>
2024-12-25 21:47:40 +0530

    Add files via upload
    Updated render server

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://foodfusion-server.onrender.com"],
   })
 );
 
