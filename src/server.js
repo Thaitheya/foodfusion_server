28154d70a69a2a24f444446ee6d4607cab3a0f20
Thaitheyasudan P K <91272336+Thaitheya@users.noreply.github.com>
2024-12-27 22:25:51 +0530

    Add files via upload
    host updated

--- src/server.js
+++ src/server.js
@@ -13,10 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
     credentials: true,
-    origin: [
-      "http://localhost:5173",
-      "https://foodfusion-server.onrender.com/",
-    ],
+    origin: ["https://foodfusion02.netlify.app/"],
   })
 );
 
