e081ffd3476c2fc026556207d9a2e3e808ee8b10
Thaitheyasudan P K <91272336+Thaitheya@users.noreply.github.com>
2024-12-30 07:39:06 +0530

    Add files via upload
    Server updated

--- src/server.js
+++ src/server.js
@@ -11,7 +11,12 @@ dbConnect();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://foodfusion02.netlify.app",
+    credentials: true,
+  })
+);
 
 app.use("/api/foods", foodRouter);
 app.use("/api/users", userRouter);
@@ -21,11 +26,6 @@ app.use("/uploads", express.static(path.join(__dirname, "public")));
 
 const publicFolder = path.join(__dirname, "public");
 app.use(express.static(publicFolder));
-
-app.get("*", (req, res) => {
-  const indexFilePath = path.join(publicFolder, "index.html");
-  res.sendFile(indexFilePath);
-});
 const PORT = 5000;
 
 app.listen(PORT, () => {
